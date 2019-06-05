6965ac14f26189766af43d010551b68679c1e431
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-05 17:58:53 +0200

    vamtiger-node-typescript-commit

--- build/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ScriptType;
-(function (ScriptType) {
-    ScriptType["jsonLd"] = "application/ld+json";
-    ScriptType["json"] = "application/json";
-})(ScriptType = exports.ScriptType || (exports.ScriptType = {}));
-exports.url = 'https://unpkg.com/vamtiger-mental-health-botswana-2018-json-ld@latest';
-//# sourceMappingURL=types.js.map
