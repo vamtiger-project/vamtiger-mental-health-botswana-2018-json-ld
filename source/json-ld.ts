5d2077c9e8f565adf25df4cdbc16b42e497298bf
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-07 22:41:26 +0200

    vamtiger-node-typescript-commit

--- source/json-ld.ts
+++ source/json-ld.ts
@@ -4,11 +4,18 @@ const jsonLd = {
     name: 'Arting Health',
     description: 'Mental Health - Depression',
     datePublished: '05 June 2019',
+    about: {
+        '@context': 'http://schema.org',
+        '@type': 'Thing',
+        name: 'Infers Group - Arting Health: Science Communication and Public Engagement Project on Mental Health',
+        description: 'A collaborative science communication and public engagement project on depression between artists, scientists and community members by Infers Group'
+    },
     image: 'https://unpkg.com/vamtiger-mental-health-botswana-json-ld@latest/vamtiger-mental-health-botswana-2018-json-ld.jpg',
     founder: [
         {
             '@type': 'Person',
             name: 'Abraham Mamela',
+            description: 'Social Entrepreneur',
             disambiguatingDescription: 'Creative Concept for Arting Health, founder and CEO of Infers Group',
             hasOccupation: {
                 '@type': 'Occupation',
@@ -20,8 +27,9 @@ const jsonLd = {
         {
             '@type': 'Organization',
             name: 'Infers Group',
+            description: 'Social Entrepreneurship, Science Communication, Public Engagement, Art Science and Community Collaboration',
             url: 'http://artinghealth.infersgroup.com',
-            email: 'info(at)infersgroup.com'
+            email: 'mailto:info@infersgroup.com'
         }
     ],
     sponsor: {
