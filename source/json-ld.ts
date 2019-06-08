d1d85f92136ea6800af2a2410615c72d535061db
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-08 14:41:49 +0200

    vamtiger-node-typescript-commit

--- source/json-ld.ts
+++ source/json-ld.ts
@@ -1,7 +1,7 @@
 const jsonLd = {
     '@context': 'http://schema.org',
     '@type': 'VisualArtwork',
-    name: 'Arting Health - Botswana',
+    name: 'Infers Group - Arting Health',
     description: 'Mental Health - Depression',
     datePublished: '05 June 2019',
     about: {
@@ -11,16 +11,19 @@ const jsonLd = {
         description: 'A collaborative science communication and public engagement project on depression between artists, scientists and community members by Infers Group'
     },
     image: 'https://unpkg.com/vamtiger-mental-health-botswana-json-ld@latest/vamtiger-mental-health-botswana-2018-json-ld.jpg',
-    founder: [
+    producer: [
+        {
+            '@type': 'Organization',
+            name: 'Infers Group',
+            description: 'Social Entrepreneurship, Science Communication, Public Engagement, Art Science and Community Collaboration',
+            url: 'http://artinghealth.infersgroup.com',
+            email: 'mailto:info@infersgroup.com'
+        },
         {
             '@type': 'Person',
             name: 'Abraham Mamela',
-            description: 'Social Entrepreneur',
-            disambiguatingDescription: 'Creative Concept for Arting Health, founder and CEO of Infers Group',
-            hasOccupation: {
-                '@type': 'Occupation',
-                name: 'Science Communicator, Project Lead for Arting Health and CEO of Infers Group'
-            }
+            description: 'Social Entrepreneurship, Science Communication and Public Engagement Professional',
+            disambiguatingDescription: 'Creative concept for Arting Health, founder and CEO of Infers Group',
         }
     ],
     creator: [
@@ -30,13 +33,20 @@ const jsonLd = {
             description: 'Social Entrepreneurship, Science Communication, Public Engagement, Art Science and Community Collaboration',
             url: 'http://artinghealth.infersgroup.com',
             email: 'mailto:info@infersgroup.com'
+        },
+        {
+            '@type': 'Person',
+            name: 'Abraham Mamela',
+            description: 'Social Entrepreneurship, Science Communication and Public Engagement Professional',
+            disambiguatingDescription: 'Creative Concept for Arting Health, founder and CEO of Infers Group',
         }
     ],
     sponsor: {
         '@type': 'Organization',
         name: 'Welcome',
         url: 'https://wellcome.ac.uk'
-    }
+    },
+    keywords: 'Infers Group, Arting Health, Art Health, Health Art, Sciart, Scientific Art, Science Art, Art Science, Science Communication, Public Engagement, Social Entrepreneurship'
 };
 
 export default [
