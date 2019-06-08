67e2241d4d5dff668bbd73f4a3465d57e2bbc475
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-08 17:29:47 +0200

    vamtiger-node-typescript-commit

--- source/json-ld.ts
+++ source/json-ld.ts
@@ -30,6 +30,7 @@ const jsonLd = {
     ],
     creator: [
         {
+            '@context': 'http://schema.org',
             '@type': 'Organization',
             name: 'Infers Group',
             description: 'Social Entrepreneurship, Science Communication, Public Engagement, Art Science and Community Collaboration',
@@ -37,17 +38,21 @@ const jsonLd = {
             email: 'mailto:info@infersgroup.com'
         },
         {
+            '@context': 'http://schema.org',
             '@type': 'Person',
             name: 'Abraham Mamela',
             description: 'Social Entrepreneurship, Science Communication and Public Engagement Professional',
             disambiguatingDescription: 'Creative Concept for Arting Health, founder and CEO of Infers Group',
         }
     ],
-    sponsor: {
-        '@type': 'Organization',
-        name: 'Welcome',
-        url: 'https://wellcome.ac.uk'
-    },
+    sponsor: [
+        {
+            '@context': 'http://schema.org',
+            '@type': 'Organization',
+            name: 'Welcome',
+            url: 'https://wellcome.ac.uk'
+        }
+    ],
     keywords: 'Infers Group, Arting Health, Art Health, Health Art, Sciart, Scientific Art, Science Art, Art Science, Science Communication, Public Engagement, Social Entrepreneurship'
 };
 
