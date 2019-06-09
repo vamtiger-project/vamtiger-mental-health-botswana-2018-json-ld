7c9d32a4656703593ca9ada088fd9edda91fea8c
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-09 21:43:20 +0200

    vamtiger-node-typescript-commit

--- source/json-ld.ts
+++ source/json-ld.ts
@@ -11,53 +11,9 @@ const jsonLd = {
         description: 'A collaborative science communication and public engagement project on depression between artists, scientists and community members by Infers Group'
     },
     image: 'https://unpkg.com/vamtiger-mental-health-botswana-json-ld@latest/vamtiger-mental-health-botswana-2018-json-ld.jpg',
-    producer: [
-        {
-            '@context': 'http://schema.org',
-            '@type': 'Organization',
-            name: 'Infers Group',
-            description: 'Social Entrepreneurship, Science Communication, Public Engagement, Art Science and Community Collaboration',
-            url: 'http://infersgroup.com',
-            email: 'mailto:info@infersgroup.com'
-        },
-        {
-            '@context': 'http://schema.org',
-            '@type': 'Person',
-            name: 'Abraham Mamela',
-            description: 'Social Entrepreneurship, Science Communication and Public Engagement Professional',
-            disambiguatingDescription: 'Creative concept for Arting Health, founder and CEO of Infers Group',
-        }
-    ],
-    creator: [
-        {
-            '@context': 'http://schema.org',
-            '@type': 'Organization',
-            name: 'Infers Group',
-            description: 'Social Entrepreneurship, Science Communication, Public Engagement, Art Science and Community Collaboration',
-            url: 'http://infersgroup.com',
-            email: 'mailto:info@infersgroup.com'
-        },
-        {
-            '@context': 'http://schema.org',
-            '@type': 'Person',
-            name: 'Abraham Mamela',
-            description: 'Social Entrepreneurship, Science Communication and Public Engagement Professional',
-            disambiguatingDescription: 'Creative Concept for Arting Health, founder and CEO of Infers Group',
-        }
-    ],
-    sponsor: [
-        {
-            '@context': 'http://schema.org',
-            '@type': 'Organization',
-            name: 'Welcome',
-            description: `
-                Wellcome exists to improve health by helping great ideas to thrive.
-                We support researchers, we take on big health challenges, we campaign for better science, and we help everyone get involved with science and health research.
-                We are a politically and financially independent foundation.
-            `,
-            url: 'https://wellcome.ac.uk'
-        }
-    ],
+    producer: true,
+    creator: true,
+    funder: true,
     keywords: 'Infers Group, Arting Health, Art Health, Health Art, Sciart, Scientific Art, Science Art, Art Science, Science Communication, Public Engagement, Social Entrepreneurship'
 };
 
--- source/json.ts
+++ source/json.ts
@@ -3,7 +3,27 @@ import {
 } from './types';
 
 const { svgCaption } = Resource;
+const producer = [
+    'https://unpkg.com/vamtiger-organization-infers-group-json-ld',
+    'https://unpkg.com/vamtiger-person-abraham-mamela-json-ld'
+];
+const creator = [
+    ...producer
+];
+const funder = [
+    'https://unpkg.com/vamtiger-organization-welcome-json-ld'
+];
 
 export default {
-    svgCaption
+    svgCaption,
+    jsonLd: [
+        {
+            index: 0,
+            fields: {
+                producer,
+                creator,
+                funder
+            }
+        }
+    ]
 };
