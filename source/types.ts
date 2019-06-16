9ae85731d2c8344e590bd7dbb53ed2ce24f6449f
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-16 21:50:32 +0200

    test

--- source/types.ts
+++ source/types.ts
@@ -17,4 +17,4 @@ export interface ILoadScript {
     data: IAnyObject;
 }
 
-export const url = 'https://cdn.jsdelivr.net/npm/vamtiger-mental-health-botswana-json-ld@latest/build/vamtiger-mental-health-botswana-2018-json-ld.js';
+export const url = 'https://vamtiger-project.github.io/vamtiger-mental-health-botswana-json-ld/build/vamtiger-mental-health-botswana-2018-json-ld.js';
